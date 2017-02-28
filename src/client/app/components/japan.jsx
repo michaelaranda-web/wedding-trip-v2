67fff8db95c5bbd14b6ceef6163b99f4e3c5dbc3
Michael Aranda <michaelaranda0.web@gmail.com>
2017-02-28 00:59:06 -0800

    Mostly Japan page content and styling + various general changes

--- src/client/app/components/japan.jsx
+++ src/client/app/components/japan.jsx
@@ -5,7 +5,54 @@ export class Japan extends React.Component {
   render() {
     return (
       <div className="japan-page">
-        Japan
+				<div id="japan-banner" ></div>
+
+				<br />
+
+        <div className="row">
+
+	        <div className="current-location col-sm-7">
+		        <div className="current-location-body">
+		          <div className="map-frame col-sm-5">
+		            <div id="map"/>
+		          </div>
+		          <div className="details col-sm-7">
+		            <div className="location jp">TOKYO, JAPAN</div>
+		            <p>Tokyo, officially Tokyo Metropolis, is the capital of Japan and one of its 47 prefectures. The Greater Tokyo Area is the most populous metropolitan area in the world. It is the seat of the Emperor of Japan and the Japanese government.</p>
+		          </div>
+		        </div>
+		      </div>
+
+					<div className="col-xs-5">
+						<div className="totoro" />
+					</div>		      
+
+		      <div className="col-xs-12" />
+
+		      <div className="tile-side-buffer col-xs-3"></div>
+		      <div className="long-tile col-xs-9">
+		      	<h4 className="title-right">STUFF</h4>
+		      	<div className="tile-body"></div>
+		      </div>
+
+		      <div className="col-xs-12" />
+
+		      <div className="long-tile col-xs-9">
+		      	<h4 className="title-left">MORE STUFF</h4>
+		      	<div className="tile-body"></div>
+		      </div>
+
+		      <div className="tile-side-buffer col-xs-3"></div>
+
+        </div>
+
+        <br />
+        <br />
+
+        <div className="col-sm-12 section-divider">
+        	<div className="cat-icon" />
+        </div>
+
       </div>
     );
   }
