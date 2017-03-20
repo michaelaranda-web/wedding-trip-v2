4efa797cdea36023ee9efeb694c3fa34ebc805e3
Michael Aranda <michaelaranda0.web@gmail.com>
2017-03-19 21:12:50 -0700

    Add section for wedding cake icon links on home page.

--- src/client/app/components/home.jsx
+++ src/client/app/components/home.jsx
@@ -8,7 +8,7 @@ export class Home extends React.Component {
         <div className="bride-and-groom col-sm-4" />
         <div className="home-content col-sm-8">
         	<h1>Martina and Michael's Wedding Trip 2017</h1>
-        	<h3></h3>
+        	<div className="wedding-cakes-table">Wedding Cakes Table Placeholder</div>
         </div>
         <div className="bottom-section col-sm-12">
           <div className="welcome-message"></div>
