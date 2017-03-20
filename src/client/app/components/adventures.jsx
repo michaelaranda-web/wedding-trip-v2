06a34d338c00990452fb171fbc1a4f5d728d6fdf
Michael Aranda <michaelaranda0.web@gmail.com>
2017-03-19 23:53:19 -0700

    Adds page header and background to Adventures page.

--- src/client/app/components/adventures.jsx
+++ src/client/app/components/adventures.jsx
@@ -4,8 +4,12 @@ export class Adventures extends React.Component {
  
   render() {
     return (
-      <div className="adventures-page">
-        ADVENTURE!
+      <div className="adventures-page col-xs-12">
+        <div className="header-section">
+          <div className="col-sm-3 title-left">Our</div>
+          <div className="floating-heads col-sm-3"></div>
+          <div className="col-sm-6 title-right">Adventures!</div>
+        </div>
       </div>
     );
   }
