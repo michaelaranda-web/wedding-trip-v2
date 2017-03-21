f4b57fb509e0d7ca3744b1514329527dbe56740a
Michael Aranda <michaelaranda0.web@gmail.com>
2017-03-21 00:37:01 -0700

    Adds story items to story page with hover effect.

--- src/client/app/components/story.jsx
+++ src/client/app/components/story.jsx
@@ -4,7 +4,24 @@ export class Story extends React.Component {
   render() {
     return (
         <div className="story-page">
-          OUR STORY!
+          <h1>Our Story</h1>
+          <div className="story-items">
+            <div className="story-item col-md-4 col-sm-6">Test</div>
+            <div className="story-item col-md-4 col-sm-6">Test</div>
+            <div className="story-item col-md-4 col-sm-6">Test</div>
+            <div className="story-item col-md-4 col-sm-6">Test</div>
+            <div className="story-item col-md-4 col-sm-6">
+              CAN YOU SEE ME?
+              <div className="story-item-image"></div>
+            </div>
+            <div className="story-item col-md-4 col-sm-6">Test</div>
+            <div className="story-item col-md-4 col-sm-6">Test</div>
+            <div className="story-item col-md-4 col-sm-6">Test</div>
+            <div className="story-item col-md-4 col-sm-6">Test</div>
+            <div className="story-item col-md-4 col-sm-6">Test</div>
+            <div className="story-item col-md-4 col-sm-6">Test</div>
+            <div className="story-item col-md-4 col-sm-6">Test</div>
+          </div>
         </div>
     );
   }
