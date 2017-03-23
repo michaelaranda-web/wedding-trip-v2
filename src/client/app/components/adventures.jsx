2e565f0e672826103b36196db668709441735b6a
Michael Aranda <michaelaranda0.web@gmail.com>
2017-03-22 22:26:25 -0700

    Finish styling for Adventures page + icon link functionality.

--- src/client/app/components/adventures.jsx
+++ src/client/app/components/adventures.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 
 export class Adventures extends React.Component {
  
@@ -13,18 +14,27 @@ export class Adventures extends React.Component {
 
         <div className="adventure-icons-section">
           <div className="col-xs-12 col-md-4 icon">
-            <div className="icon-image japan"></div>
+            <LinkContainer to={{ pathname: 'japan'}}>
+              <div className="icon-image japan"></div>
+            </LinkContainer>
             <div className="icon-title">JAPAN</div>
+            <div className="icon-subtitle">April 16 - April 20</div>
           </div>
 
           <div className="col-xs-12 col-md-4 icon">
-            <div className="icon-image taiwan"></div>
+            <LinkContainer to={{ pathname: 'taiwan'}}>
+              <div className="icon-image taiwan"></div>
+            </LinkContainer>
             <div className="icon-title">TAIWAN</div>
+            <div className="icon-subtitle">April 21 - April 25</div>
           </div>
 
           <div className="col-xs-12 col-md-4 icon">
-            <div className="icon-image hawaii"></div>
+            <LinkContainer to={{ pathname: 'hawaii'}}>
+              <div className="icon-image hawaii"></div>
+            </LinkContainer>
             <div className="icon-title">HAWAII</div>
+            <div className="icon-subtitle">April 26 - April 30</div>
           </div>
         </div>
       </div>
